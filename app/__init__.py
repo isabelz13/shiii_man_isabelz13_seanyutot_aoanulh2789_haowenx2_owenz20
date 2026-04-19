77354694f40dcda98ab39fa07fa8ae415bb47a18
Aoanul Hoque <aoanulh2789@nycstudents.net>
2026-04-19 17:23:40 -0400

    upd

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ import folium
 import utility
 from pathlib import Path
 import json
+import pandas
 
 app = Flask(__name__)
 app.secret_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"
@@ -57,17 +58,38 @@ def build_map():
 
     election_path = data_folder / "nyed_18d.geojson"
     zipcode_path = data_folder / "ZipCode.geojson"
+    income_path = data_folder / "IncomeData.csv"
+
+    income_data = pandas.read_csv(income_path)
 
     with open(election_path, "r", encoding="utf-8") as f:
         election_data = json.load(f)
 
     with open(zipcode_path, "r", encoding="utf-8") as f:
         zipcode_data = json.load(f)
+
     zipcode_data = clean_geojson(zipcode_data)
 
-    # Election districts layer
-    election_fg = folium.FeatureGroup(name="Election Districts", show=True)
+    income_data["ZIP"] = (
+    income_data["ZIP"]
+    .astype(str)
+    .str.replace(".0", "", regex=False)
+    .str.strip()
+    .str.zfill(5)
+    )
 
+    election_fg = folium.FeatureGroup(name="Election Districts", show=True)
+    income_data["MedianIncome"] = pandas.to_numeric(income_data["MedianIncome"], errors="coerce")
+    income_lookup = dict(zip(income_data["ZIP"], income_data["MedianIncome"]))
+
+    for feature in zipcode_data["features"]:
+        zip_code = str(feature["properties"].get("modzcta", "")).zfill(5)
+       # print(zip_code)
+        # print(income_lookup)
+        income = income_lookup.get(zip_code)
+        
+
+        feature["properties"]["MedianIncome"] = "$" + str(income)
     folium.GeoJson(
         election_data,
         style_function=lambda feature: {
@@ -75,29 +97,49 @@ def build_map():
             "color": "#3388ff",
             "weight": 1,
             "fillOpacity": 0.2
-        },
-
+        }
     ).add_to(election_fg)
 
     election_fg.add_to(m)
 
-    zipcode_fg = folium.FeatureGroup(name="Zip Codes", show=False)
-
+    folium.Choropleth(
+        geo_data=zipcode_data,
+        data=income_data,
+        columns=["ZIP", "MedianIncome"],
+        key_on="feature.properties.modzcta",
+        fill_color="YlGnBu",
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        nan_fill_color="gray",
+        legend_name="Median Income",
+        name="Zip Code and Median Income",
+        show=False
+    ).add_to(m)
     folium.GeoJson(
         zipcode_data,
+        name="Zip Code ToolTip",
         style_function=lambda feature: {
-            "fillColor": "#7aff99",
-            "color": "#418952",
+            "fillColor": "transparent",
+            "color": "black",
             "weight": 1,
-            "fillOpacity": 0.15
+            "fillOpacity": 0
         },
-    ).add_to(zipcode_fg)
-
-    zipcode_fg.add_to(m)
-
+        highlight_function=lambda feature: {
+            "weight": 3,
+            "color": "yellow",
+            "fillOpacity": 0.1
+        },
+        tooltip=folium.GeoJsonTooltip(
+            fields=["modzcta", "MedianIncome"],
+            aliases=["ZIP Code:", "Median Income:"],
+            labels=True,
+            sticky=False
+        )
+    ).add_to(m)
     folium.LayerControl().add_to(m)
 
     return m
+
 @app.get('/')
 def home_get():
     m = build_map()
